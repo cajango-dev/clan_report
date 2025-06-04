c8627f99714aa57e9a938d34dbc1d8e62db35118
cajango-dev <cajango.dev@gmail.com>
2025-06-04 01:46:22 -0300

    commit 04/06

--- discord_bot.py
+++ discord_bot.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1379643507894915102/jUtfOdLN6eXbi7OSjtHM01ae3OrzxyYFsNv4qAPUh6BfQ7XWYuOB_scGZZH7ryCu31cF"
 
-THUMBNAIL_URL = "https://cdn.discordapp.com/attachments/1376607654201524318/1379453065567932489/image.png?ex=6840f407&is=683fa287&hm=f92c33ea86a27614fcee35494b048df8b398467feefa290e3c06c5c4592887c3&"
-FOOTER_ICON_URL = "https://cdn.discordapp.com/attachments/1376607654201524318/1379453065567932489/image.png?ex=6840f407&is=683fa287&hm=f92c33ea86a27614fcee35494b048df8b398467feefa290e3c06c5c4592887c3&"
+THUMBNAIL_URL = "https://cdn.discordapp.com/attachments/1376607654201524318/1379675803310362664/tgkcred.png?ex=68411ab8&is=683fc938&hm=65e51229cd721cf1099d4ff33e47e2eabb46774f9e1340f1baa82eda7d46362a&"
+FOOTER_ICON_URL = "https://cdn.discordapp.com/attachments/1376607654201524318/1379675803310362664/tgkcred.png?ex=68411ab8&is=683fc938&hm=65e51229cd721cf1099d4ff33e47e2eabb46774f9e1340f1baa82eda7d46362a&"
 
 def send_clan_report(players):
     print("Enviando dados para Discord...")
@@ -14,7 +14,7 @@ def send_clan_report(players):
         embed = {
             "title": f"Relatório do Clã - Jogadores {i + 1} a {min(i + 10, len(players))}",
             "description": "",
-            "color": 0x00adff,
+            "color": 0xff0000,
             "thumbnail": {
                 "url": THUMBNAIL_URL
             },
